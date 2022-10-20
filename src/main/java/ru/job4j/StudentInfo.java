e60693e2b0c681d3ea0dada307628fb987e93359
decrent <mortalkombinat@gmail.com>
2022-10-20 10:04:38 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Pikalov");
+        System.out.println("04.01.1990");
+    }
+}
