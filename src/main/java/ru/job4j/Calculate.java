895008d2b1cae1e490779726b3cf439277dec9a1
decrent <mortalkombinat@gmail.com>
2022-10-20 20:48:46 +0700

    3. Арифметические операции. [#185]

---
+++ src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Calculate {
+        public static void main(String[] args) {
+            int age = 18;
+            System.out.println(age);
+            double lenght = 78.0332;
+            System.out.println(lenght);
+            boolean baby = true;
+            System.out.println(baby);
+            long bacteriumAmount = 969552987;
+            System.out.println(bacteriumAmount);
+        }
+}
