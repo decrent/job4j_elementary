03d800ce7f76784cf7798bf826d08d56f4ed11a2
decrent <mortalkombinat@gmail.com>
2022-10-22 01:12:17 +0700

    0. Что такое тестирование [#159431]

---
+++ src/main/java/ru/job4j/klass1.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+public class klass1 {
+    public static void main(String[] args) {
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(b);
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
+        char c = 45000;
+        float z = c;
+        System.out.println(z);
+        double d = 121.19;
+        byte bb = (byte) d;
+        System.out.println(bb);
+        short ss = 1500;
+        char cc = (char) ss;
+        double dd = cc;
+        System.out.println(dd);
+    }
+}
