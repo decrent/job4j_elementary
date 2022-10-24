b2da53f9a401d957ee293cb3082fb88a65c3a05f
decrent <mortalkombinat@gmail.com>
2022-10-24 09:46:01 +0700

    2. Тесты для задачи "идеальный вес" [#159458 #354221]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -14,7 +14,7 @@ public class FitTest {
     }
 
     @Test
-    public void whenWoman187Then8855() {
+    public void whenWoman187Then88dot55() {
         short in = 187;
         double expected = 88.55;
         double out = Fit.womanWeight(in);
