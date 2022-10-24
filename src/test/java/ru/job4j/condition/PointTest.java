a1a1032a6700b7a8a5419cb402404f98cfb926d0
decrent <mortalkombinat@gmail.com>
2022-10-24 09:54:25 +0700

    3. Тесты для определения расстояния между точками. [#159459 #354235]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -3,51 +3,49 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class PointTest {
 
     @Test
-    public void when47To1216Then1204() {
+    public void when47To1216Then12dot04() {
         int x1 = 4;
-        int x2 = 7;
-        int y1 = 12;
+        int y1 = 7;
+        int x2 = 12;
         int y2 = 16;
-        double expetcted = 12.04;
-        double out = Point.distance(x1, x2, y1, y2);
-        Assert.assertEquals(expetcted, out, 0.01);
-    }
-
-    @Test
-    public void when35To40Then509() {
-        int x1 = 3;
-        int x2 = 5;
-        int y1 = 4;
-        int y2 = 0;
-        double expected = 5.09;
-        double out = Point.distance(x1, x2, y1, y2);
+        double expected = 12.04;
+        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when1377To2288Then14212() {
+    public void when35To40Then5dot09() {
+        int x1 = 3;
+        int y1 = 5;
+        int x2 = 4;
+        int y2 = 0;
+        double expected = 5.09;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when1377To2288Then14dot212() {
         int x1 = 13;
-        int x2 = 77;
-        int y1 = 22;
+        int y1 = 77;
+        int x2 = 22;
         int y2 = 88;
         double expected = 14.212;
-        double out = Point.distance(x1, x2, y1, y2);
+        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.001);
     }
 
     @Test
-    public void when1167To9999Then93637599() {
+    public void when1167To9999Then93dot637599() {
         int x1 = 11;
-        int x2 = 67;
-        int y1 = 99;
+        int y1 = 67;
+        int x2 = 99;
         int y2 = 99;
         double expected = 93.637599;
-        double out = Point.distance(x1, x2, y1, y2);
+        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.00001);
     }
 }
