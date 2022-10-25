a43f60b85cef3281362d87fb7595e4c7822d8bb0
decrent <mortalkombinat@gmail.com>
2022-10-25 22:18:39 +0700

    4.4. Вложенный оператор if else. Запутанный код [#190]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,13 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Artem Pikalov");
-        System.out.println("04.01.1990");
+    public class Main {
+        public static void main(String[] args) {
+            int rsl = 18 % 7;
+            System.out.println("Остаток от деления чисел 18 и 7 равен: " + rsl);
+            System.out.println();
+            rsl = 18 % 6;
+            System.out.println("Остаток от деления чисел 18 и 6 равен: " + rsl);
+        }
     }
 }
