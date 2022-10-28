32d2958fc28835c3dd6ac5c2b47372c6498e7c6d
decrent <mortalkombinat@gmail.com>
2022-10-28 11:08:46 +0700

    5.4. Протеиновая диета [#156313]

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -2,8 +2,8 @@ package ru.job4j.loop;
 
 public class Board {
     public static void paint(int width, int height) {
-        for (int row = 1; row <= height; row++) {
-            for (int cell = 1; cell <= width; cell++) {
+        for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
                 if ((cell + row) % 2 == 0) {
                     System.out.print("X");
                 } else {
---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nick) {
+        int month = 0;
+        while (ivan <= nick) {
+            month += 1;
+            ivan *= 3;
+            nick *= 2;
+            System.out.println(month);
+        }
+        return month;
+    }
+}
