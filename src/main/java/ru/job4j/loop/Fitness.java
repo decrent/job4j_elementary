5095f7217ae4c60abd4fc12af7d2f610e97cb637
decrent <mortalkombinat@gmail.com>
2022-10-28 15:46:42 +0700

    5.4. Протеиновая диета [#156313 #355645]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,7 +4,7 @@ public class Fitness {
     public static int calc(int ivan, int nick) {
         int month = 0;
         while (ivan <= nick) {
-            month += 1;
+            month++;
             ivan *= 3;
             nick *= 2;
             System.out.println(month);
