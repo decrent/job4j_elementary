93b40fa2eb0eaf391af47d7a1e05118bd80a48af
decrent <mortalkombinat@gmail.com>
2022-10-30 12:56:58 +0700

    5.7. Ипотека [#156315 #355941]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        percent = percent / 100 + 1;
-        while (amount * percent > 0) {
-            amount *= percent;
-            amount -= salary;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
