3e12f6a848a3576eae3425400821929d4dfca971
decrent <mortalkombinat@gmail.com>
2022-10-30 14:00:34 +0700

    5.9.1. Генерация конструкций в IDEA [#279134 #355954]

--- src/main/java/ru/job4j/live/Loop.java
+++ src/main/java/ru/job4j/live/Loop.java
@@ -1,12 +1,9 @@
 package ru.job4j.live;
 
-public class Main {
+public class Loop {
     public static void main(String[] args) {
-        System.out.println();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
     }
 }
-
-
