d7424c1a1bf2bca1d1d0b24117510455a3cb443d
decrent <mortalkombinat@gmail.com>
2022-10-31 10:15:33 +0700

    6.4.3. Слово заканчивается на ... [#156317 #356348]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,16 +2,11 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
-        for (int i = 0; i < word.length; i++) {
-            if (i == post.length) {
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - i - 1] != post[post.length - i - 1]) {
+                result = false;
                 break;
-            }
-
-            if (word[word.length - i - 1] == post[post.length - i - 1]) {
-                result = true;
-            } else {
-                return result;
             }
         }
         return result;
