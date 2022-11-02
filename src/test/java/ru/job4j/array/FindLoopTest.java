64709fffc04a7bc8d6d33072caa916ee9e84f0b3
decrent <mortalkombinat@gmail.com>
2022-11-02 17:57:47 +0700

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,12 +1,13 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -15,7 +16,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[] {3, 4, 5, 23, 21, 1, 6};
+        int[] data = new int[]{3, 4, 5, 23, 21, 1, 6};
         int el = 10;
         int result = FindLoop.indexOf(data, el);
         int expected = -1;
@@ -24,7 +25,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas43Then7() {
-        int[] data = new int[] {1, 2, 3, 4, 7, 42, 12, 43, 35};
+        int[] data = new int[]{1, 2, 3, 4, 7, 42, 12, 43, 35};
         int el = 43;
         int result = FindLoop.indexOf(data, el);
         int expected = 7;
@@ -33,10 +34,55 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas228Then3() {
-        int[] data = new int[] {23431, 657, 45, 228, 789, 3476, 1, 23, 5};
+        int[] data = new int[]{23431, 657, 45, 228, 789, 3476, 1, 23, 5};
         int el = 228;
         int result = FindLoop.indexOf(data, el);
         int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+
+    @Test
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[]{5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int el = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[]{5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[]{1, 8, 2, 3, 4, 5, 6, 7, 9, 10};
+        int el = 8;
+        int start = 2;
+        int finish = 9;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
 }
